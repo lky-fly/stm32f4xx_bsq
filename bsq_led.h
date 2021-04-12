2deac278836f0d0de23c84b77f4c8db4439451c3
lky-fly <75171687+lky-fly@users.noreply.github.com>
2021-04-12 17:25:48 +0800

    Create bsq_led.h
    led版级支持包的头文件

---
+++ bsq_led.h
@@ -0,0 +1,7 @@
+#ifndef _BSQ_LED_H//养成条件编译的习惯
+#define _BSQ_LED_H
+void gpio_led_config(void);
+void bsq_led_open(void);//打开led灯
+void bsq_led_off(void);//关闭led灯
+//void gpio_led_controll();
+#endif
